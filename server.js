28f1876fa6706fedfd8b8c384c1be8de73f2380f
Naveen <nrajr23@gmail.com>
2022-11-26 11:08:48 +0530

    updated

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv=require('dotenv')
 
 //load env files
 //config()method help to path for env
-dotenv.config({path:'config/.env'})
+dotenv.config()
 
 const app=express();
 const PORT=process.env.PORT || 6969
